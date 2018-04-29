39123391fa7b06c77db9fc134aa490af7e657a96
Jourdan Luca <luca.jourdan@etu.univ-lyon1.fr>
2018-04-29 20:28:50 +0200

    amelioration des performances + pris en charge multicoeur pour la transformation

--- NetClustering.py
+++ NetClustering.py
@@ -2,6 +2,7 @@ import math
 from igraph import Graph as ig
 from graph_tool.all import minimize_nested_blockmodel_dl, Graph as gt
 import os
+import pickle
 
 
 # ------------------------ distance
@@ -55,30 +56,62 @@ class netMethods:
 	# ------------------------ transfrom
 	class transform:
 
-		#k nearest neightboors
+		#k nearest neightboors (! O(n²))
 		def naiveTransform(netData,**kwargs):
 			k = kwargs.get('k', None)	# valeur par défault à definir, None ne peut pas permettre de construire un graphe
-			multiCore = kwargs.get('multiCore', True)
+			nbrCore = kwargs.get('Core', 6)
 			nbrPoint = len(netData)
+			nbrPointCore = nbrPoint//nbrCore
 			distance = dict()
 			graph = []
 
-			for i in range(nbrPoint) :
-				j = 0
+			def partialTransform(debut, fin) :
+				for i in range(debut, fin) :
+					j = 0
+					#calcul of all the distances :
+					while j < nbrPoint :
+						distance[j] = euclidianDistance(netData[i], netData[j])
+						j += 1
 
-				#calcul of all the distances :
-				while j < nbrPoint :
-					distance[j] = euclidianDistance(netData[i], netData[j])
-					j += 1
+					#construction of the graph :
+					j = 0
+					while j < k :
+						del distance[i]
+						nearest = min(distance, key=distance.get)
+						del distance[nearest]	#if k > 1 we don't want to get always the same point.
+						if nearest < i :	#the other if is impossible if nearest < i and this test is faster.
+							if([nearest, i] not in graph) :	#as the graph is non-oriented we don't want to add 2 time the same edge.
+								graph.append([i, nearest])
+						j += 1
 
-				#construction of the graph :
-				j = 0
-				while j < k :
-					nearest = min(distance)
-					del distance[nearest]	#if k > 1 we don't want to get always the same point.
-					if([nearest, i] not in graph) :	#as the graph is non-oriented we don't want to add 2 time the same edge.
-						graph.append([i, nearest])
-					j += 1
+				return graph
+
+			#pipes
+			r = [-1,-1]*nbrCore
+			w = [-1,-1]*nbrCore
+			pid = [-1]*nbrCore
+
+			for i in range(nbrCore):
+				r[i], w[i] = os.pipe()
+
+				try:
+				    pid[i] = os.fork()
+				except OSError:
+				    exit("Could not create a child process\n")
+
+
+				if pid[i] == 0:
+					if i < nbrCore-1 :
+						g = partialTransform(i*nbrPointCore, (i+1)*nbrPointCore)
+					else :
+						g = partialTransform(i*nbrPointCore, nbrPoint)	#to be sure that there is not a forgoten point.
+					os.write(w[i], pickle.dumps(g))
+					exit()
+
+
+			for i in range(nbrCore):
+				finished = os.waitpid(pid[i], 0)
+				graph += pickle.loads(os.read(r[i], 250000000))
 
 			return graph
 
