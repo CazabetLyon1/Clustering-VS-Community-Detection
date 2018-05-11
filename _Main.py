3e4cc34f3e20cae37f8ae187f02049350abc624e
Jourdan Luca <luca.jourdan@etu.univ-lyon1.fr>
2018-05-11 09:24:17 +0200

    main calcul resultat attToNet

--- _Main.py
+++ _Main.py
@@ -1,29 +1,41 @@
 import ClusterLib
+import csv
 
-# --- Load Data
-print('loading...')
-attr1 = ClusterLib.loadData_Att('data/att/random.csv')
-net = ClusterLib.loadData_Net('data/net/karate.gml')
-print('net -> att')
-attr = ClusterLib.Net2Att(net,ClusterLib.attMethods.transform.laplacianEigenmaps)
-print(attr)
+nbrCore = 6
 
-net = ClusterLib.Att2Net(attr,ClusterLib.netMethods.transform.naiveTransform,k=5)
-# --- Clustering
-print('Clustering...')
-method = ClusterLib.attMethods.clustering.kMeans
-cluster = ClusterLib.cluster_Att(attr,method,3)
+# --- Load Data :
+#data :
+attrFile = ["data/att/s2", "data/att/birch1", "data/att/birch2", "data/att/dim032", "data/att/dim064", "data/att/dim128", "data/att/dim256", "data/att/s1", "data/att/s3" ,"data/att/s4"]
+netwFile = ["data/net/chaine", "data/net/dolphins", "data/net/football", "data/net/interconnexion_totale", "data/net/karate", "data/net/clique"]
 
-print(cluster)
-# --- Plotting
-print('Plotting...')
-import matplotlib.pyplot as plt
-plt.ion()
-colors = ['green','red','blue','magenta','yellow']
-for i in range(len(cluster)):
-	xData = attr[i][0]
-	yData = attr[i][1]
-	clusterId = cluster[i]
-	col = colors[clusterId]
-	plt.scatter(xData,yData,color=col)
-plt.pause(100)
+print("loading ....")
+for i in range(len(attrFile)) :
+	#load data.
+	attr = ClusterLib.loadData_Att(attrFile[i]+".csv")
+	#load ground truth.
+	temp = csv.reader(open(attrFile[i] + "_labels.csv","r"))
+	groundTruth = []
+	for row in temp: groundTruth.append(int(row[0]))
+
+	#on teste pour différents k.
+	for k in range(1,11) :
+		methodNet = ["infomap", "louvain", "labelPropagation", "nestedBlockmodel"]
+		print('att -> net ' + str(k))
+		#transformation.
+		net = ClusterLib.Att2Net(attr,ClusterLib.netMethods.transform.naiveTransform,k=k, Core=nbrCore)
+		#calcule f1score et AMI pour chaque méthode.
+		for m in methodNet :
+			print(m)
+			method = getattr(ClusterLib.netMethods.clustering, m)
+			cluster = ClusterLib.cluster_Net(net, method)
+			f1 = ClusterLib.Metric.f1(groundTruth,cluster)
+			ami = ClusterLib.Metric.ami(groundTruth,cluster)
+			print("avec le fichier " + attrFile[i] + " et pour k = " + str(k) + " : \nami = " + str(ami) + "\nf1score = " + str(f1))
+			mon_fichier = open("resultat/fichier=" + str(i) + "_k=" + str(k) + "_method=" + m + ".txt", "w")
+			mon_fichier.write("f1 = " + str(f1) + "\n")
+			mon_fichier.write("ami = " + str(ami) + "\n")
+			mon_fichier.write("\n network :\n\n")
+			mon_fichier.write(str(net))
+			mon_fichier.write("\n \n \n \n cluster :\n\n")
+			mon_fichier.write(str(cluster))
+			mon_fichier.close()
