5e328e0cf784ab160616ea89e842fd4a165e80dc
Jourdan Luca <luca.jourdan@etu.univ-lyon1.fr>
2018-05-18 10:46:20 +0200

    amelioration vitesse calcul f1score

--- Metric.py
+++ Metric.py
@@ -1,16 +1,19 @@
 from sklearn.metrics import f1_score, adjusted_mutual_info_score as AMI
 from sklearn.preprocessing import MultiLabelBinarizer
+import numpy as np
 
 
 def f1(trueClusters, predClusters, **kwargs) :
     avg = kwargs.get('average', "macro")   # accept this values : None, ‘binary’ (default), ‘micro’, ‘macro’, ‘samples’, ‘weighted’
     nbrClusterPredicted = max(predClusters)
     maxScore = 0
-    for i in range(0, nbrClusterPredicted+1) :  #f1_score() preserve the order of the cluster but it's not what we want here.
-        clusters = predClusters
-        for elem in range(len(clusters)) :
-            clusters[elem] = (clusters[elem] + i) % (nbrClusterPredicted+1)
-        score = f1_score(trueClusters, clusters, average=avg)
+    pred = np.array(predClusters)
+
+    for i in range(0, (nbrClusterPredicted+1)) :  #f1_score() preserve the order of the cluster but it's not what we want here.
+        cluster = pred
+        cluster = cluster + i
+        cluster = cluster % (nbrClusterPredicted+1)
+        score = f1_score(trueClusters, cluster, average=avg)
         if score > maxScore :
             maxScore = score
     return maxScore
